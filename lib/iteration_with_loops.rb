45c927a67fe3bc5e53b3b19e256dc4253ecbb84c
Mar Mustafa <amustaf@email.arizona.edu>
2020-07-12 16:56:35 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,6 +7,6 @@ def find_min_in_nested_arrays(src)
   while row_index < src.length do
     element_index = 0 
     while element_index < src[row_index].length do
-      if src[row_index][element_index] < outer_results[]
+      if src[row_index][element_index] < outer_results
 
 end
